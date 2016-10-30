1f9f13fd51b663ae3e38b8bfab34b9a08bd1b522
simplydesigner <simplydesigner@ya.ru>
2016-10-30 15:56:57 +0300

    Initial Commit

---
+++ greeting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  greeting
+//
+//  Created by simplydesigner on 10/30/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
